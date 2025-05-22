b23df7a6203cbdc6b8339433373873435249d9b4
Juan Justicia <juanjusticia32@gmail.com>
2025-05-23 01:03:30 +0200

    Ajustes en archivos de configuracion para el despliegue

--- astroPlayground/astro.config.mjs
+++ astroPlayground/astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
-
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  output: 'server', 
+  adapter: node({
+    mode: 'standalone' 
+  }),
 
   vite: {
     plugins: [tailwindcss()],
